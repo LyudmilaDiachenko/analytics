edea10c935dbdb14157f22f811428db922ab1ccc
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-03-06 16:20:55 +0200

    add rate, header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import logo from "../assets/icon_logo.png"
+import Rate from "./rate"
 
 export default function Header() {
   return (
-    <div>header</div>
+    <div className='header_container'>
+      <div className='logo_container'>
+        <img src={logo} alt='logo type' className='logo_img'/>
+        <p className='logo_txt'>Macro Analytics</p>
+      </div>
+      <Rate />
+    </div>
   )
 }
---
+++ src/components/rate.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Rate() {
+  const UAH_CURR = 980
+  const USD_CURR = 840
+  const EUR_CURR = 978
+  const [date, setDate] = useState()
+  const [usdBuy, setUSDBuy] = useState()
+  const [eurBuy, setEURBuy] = useState()
+  const [usdSell, setUSDSell] = useState()
+  const [eurSell, setEURSell] = useState()
+
+  async function updateRates() {
+    const URL = `https://api.monobank.ua/bank/currency`
+    let data =  await (await fetch(URL)).json();
+    if (Array.isArray(data)) {
+      setUSDBuy(data?.find(a => a.currencyCodeA==USD_CURR && a.currencyCodeB==UAH_CURR).rateBuy)
+      setEURBuy(data?.find(a => a.currencyCodeA==EUR_CURR && a.currencyCodeB==UAH_CURR).rateBuy)
+      setUSDSell(data?.find(a => a.currencyCodeA==USD_CURR && a.currencyCodeB==UAH_CURR).rateSell)
+      setEURSell(data?.find(a => a.currencyCodeA==EUR_CURR && a.currencyCodeB==UAH_CURR).rateSell)
+      setDate(new Date().toTimeString().substr(0,5))
+    }
+  }
+  useEffect(_ => {
+    updateRates()
+    setTimeout(updateRates, 30000)
+  }, [])
+
+  return (
+    usdBuy ? <div className='marquee_infinite'>
+        <div className='marquee_container'>
+          <p className='marquee_txt'>
+            {`Курс валют на ${date} USD купівля ${usdBuy} грн / продаж ${usdSell} грн   EUR купівля ${eurBuy} грн / продаж ${eurSell} грн`}
+          </p> 
+          <p className='marquee_txt'>
+            {`Курс валют на ${date} USD купівля ${usdBuy} грн / продаж ${usdSell} грн   EUR купівля ${eurBuy} грн / продаж ${eurSell} грн`}
+          </p> 
+        </div>
+      </div> : <></>
+  )
+}

5cb639416642f9a00573574bb13caf6f899ed7d4
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-03-07 13:07:35 +0200

    add: favicon, title, date

--- src/components/comparison.jsx
+++ src/components/comparison.jsx
@@ -3,15 +3,19 @@ import { Bar } from 'react-chartjs-2'
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js"
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
+let years = []
+
 export default function Comparison() {
     const [data, setData] = useState()
+    const [yearFrom, setYearFrom] = useState(new Date().getFullYear()-15)
+    const [yearTill, setYearTill] = useState(new Date().getFullYear())
 
-    async function getData(start = 2019, end = 2023) {
+    async function getData(start = yearFrom, end = yearTill) {
         let iData =  
-            (await (await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/inflation?period=y&json&id_api=prices_price_cpi_&mcrd081=Total&start=${start}0101&end=${end}1231`)).json())
+            (await (await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/inflation?period=y&json&id_api=prices_price_cpi_&mcrd081=Total&sort=dt&order=asc&start=${start}0101&end=${end}1231`)).json())
             .filter(e => e.ku == null && e.tzep == "DTPY_");
         let rData =  
-            (await (await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/inflation?period=y&json&id_api=prices_price_ppi_&mcrd081=Total&start=${start}0101&end=${end}1231`)).json())
+            (await (await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/inflation?period=y&json&id_api=prices_price_ppi_&mcrd081=Total&sort=dt&order=asc&start=${start}0101&end=${end}1231`)).json())
             .filter(e => e.mcrk110 == "BCDE" && e.tzep == "DTDPY_");
         setData({
             labels: iData.map(e => e.dt.substr(0,4)),
@@ -21,7 +25,6 @@ export default function Comparison() {
                     data: iData.map(e => e.value),
                     borderColor: "black", 
                     borderRadius: "10", 
-                    hoverBackgroundColor: "#5932EA",
                     backgroundColor: iData.map(_ => `rgb(
                         ${Math.random() * 200 + 55}, 
                         ${Math.random() * 200 + 55}, 
@@ -32,8 +35,7 @@ export default function Comparison() {
                     label: rData[0].txt,
                     data: rData.map(e => e.value),
                     borderColor: "black", 
-                    borderRadius: "10", 
-                    hoverBackgroundColor: "#5932EA",
+                    borderRadius: "10",
                     backgroundColor: iData.map(_ => `rgb(
                         ${Math.random() * 200 + 55}, 
                         ${Math.random() * 200 + 55}, 
@@ -44,10 +46,34 @@ export default function Comparison() {
         })    
     }
     useEffect(_ => {
+        years = []
+        for (let i = new Date().getFullYear(); i >= new Date().getFullYear() - 15; i--){ 
+            years.push(i)
+        }
         getData()
     }, [])
+
+    useEffect(_ => {
+        getData(yearFrom, yearTill)
+    }, [yearFrom, yearTill])
     return (
         <div>
+            <div className='first_screen'>
+                <h2 className='h2_first_screen'>Співвідношення індексів інфляції</h2>
+                <div className='select_container'>
+                    <select onChange={e => setYearFrom(e.target.value)} className='year_select'>
+                        {years.map(i => 
+                            <option value={i} key={'setYearFrom'+i} selected={yearFrom==i?'selected':false}>{i}</option>    
+                        )}
+                    </select>
+                    <select onChange={e => setYearTill(e.target.value)} className='year_select'>
+                        {years.map(i => 
+                            <option value={i} key={'setYearTill'+i} selected={yearTill==i?'selected':false}
+                            >{i}</option>    
+                        )}
+                    </select>
+                </div>
+            </div>  
             {data ? <Bar data={data} /> : <></>}
         </div>
     )
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import logo from "../assets/icon_logo.png"
+import React from 'react'
+import logo from "../assets/icon_logotype.png"
 import Rate from "./rate"
-
+import logoType from "../assets/icons8-results-100.png"
 export default function Header() {
   return (
     <div className='header_container'>
       <div className='logo_container'>
-        <img src={logo} alt='logo type' className='logo_img'/>
-        <p className='logo_txt'>Macro Analytics</p>
+        <img src={logoType} alt='Macro Analitycs' className='logo_img'/>
+        <p className='logo_txt'>Macro Analitycs</p>
       </div>
       <Rate />
     </div>

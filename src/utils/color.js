c57edd0d5b60ba1e87c589d8cbb1d74f52b1164d
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-03-08 00:21:34 +0200

    add utils/color

---
+++ src/utils/color.js
@@ -0,0 +1,7 @@
+export function getColor(item_count_in_group = 20, current_index_in_group = Math.round(Math.random()*20)){
+    let 
+        h = Math.round(255 / item_count_in_group * current_index_in_group),
+        s = Math.round(70 + Math.random() * 30),
+        l = Math.round(45 + Math.random() * 30)
+    return [`hsl(${h} ${s}% ${l}%)`, `hsl(${h} ${Math.round(s*0.9)}% ${Math.round(l*1.1)}% / 40%)`]
+}

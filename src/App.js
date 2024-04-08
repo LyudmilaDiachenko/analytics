32c4ace2af8201b0538fed2e9a3258f5d9a62f15
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-04-08 13:45:14 +0300

    add monobank picture and media file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './media.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppProvider } from './utils/context';
 import Layout from './components/layout';

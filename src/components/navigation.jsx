d894056754d7e456475e8bf365623382cc4e8493
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-04-09 13:33:15 +0300

    first commit

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -6,9 +6,9 @@ function scrollTo(e){
 export default function Navigation() {
   return (
     <div className='navigation'>
-      <a onClick={e => scrollTo(e)} data-url='#comparison'>Індекс споживчих цін / цін виробників</a>
+      <a onClick={e => scrollTo(e)} data-url='#comparison'>ІСЦ / цін виробників</a>
       <a onClick={e => scrollTo(e)} data-url='#correlation'>Співвідношення товарних груп</a>
-      <a onClick={e => scrollTo(e)} data-url='#details'>Порівняння по регіонах</a>
+      <a onClick={e => scrollTo(e)} data-url='#details'>ІСЦ по регіонах</a>
       <a onClick={e => scrollTo(e)} data-url='#industrial'>Порівняння по регіонах</a>
     </div>
   )
